07969a6561b8bc551a3c72e39b62a88b40270228
Kenmoe-donas <Kenteldonas@hotmail.com>
2025-01-24 07:19:09 +0100

    new push2

--- userdt.sh
+++ userdt.sh
@@ -1,7 +1,21 @@
+
+# #!/bin/bash
+# sudo su
+# yum update -y
+# yum install httpd -y
+# systemctl start httpd
+# systemctl enable httpd
+# echo "<html><head><style>body { background-color: purple; color: white; }</style></head><body><h1><p> Welcome to Utrains.<br> You are redirected to ${HOSTNAME} to see how the load balancer is sharing the traffic.</p></h1></body></html>" > /var/www/html/index.html
+
+
 #!/bin/bash
-sudo su
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-echo "<html><head><style>body { background-color: purple; color: white; }</style></head><body><h1><p> Welcome to Utrains.<br> You are redirected to ${HOSTNAME} to see how the load balancer is sharing the traffic.</p></h1></body></html>" > /var/www/html/index.html
+sudo yum install wget httpd mysql php php-mysql -y
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzf latest.tar.gz
+sudo cp -r wordpress/* /var/www/html
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+sudo cd /var/www/html
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sudo chown -R apache:apache /var/www/html
+sudo systemctl start httpd 
+sudo systemctl enable httpd
